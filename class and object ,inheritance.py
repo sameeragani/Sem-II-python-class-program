e9b6aacf2af0234e260c17fd84754ca49bcce0ff
sameeragani <sameeragani16@gmail.com>
2024-11-29 10:22:04 +0530

    Add files via upload

--- class and object ,inheritance.py
+++ class and object ,inheritance.py
@@ -1,4 +1,4 @@
-#using inheritance employee id
+"""#using inheritance employee id(parent and child class)
 class Employee:
     def getEmployeeInfo(self):
         self.id=input("Enter the ID:")
@@ -25,7 +25,7 @@ class Inventory:
 a=Inventory(12335 ,"arins", 10)
 a.Display()
 
-#test modified to inheritance
+#test modified to inheritance(parent class and child class)g
 class Inventory:
     def _init_(self):
         self.prodid=int(input('enter Id'))
@@ -37,4 +37,31 @@ class Display(Inventory):
         print('Product name',self.prodid,'product name',self.prodname,'product count',self.prodcount)
     
 d=Display()
-d.show()
+d.show()"""
+
+
+#multiple inheritance(many parent one child )
+class Employee:
+    def __init__(self,name,Id,position):
+        self.name=name
+        self.Id=Id
+        self.position=position
+    def displayEmployeeInfo(self):
+        print(f"Name:{self.name}\nId:{self.Id}\nPosition:{self.position}")
+class Address:
+    def __init__(self,street,state,country):
+        self.street=street
+        self.state=state
+        self.country=country
+    def displayaddressInfo(self):
+        print(f"Street Name: {self.street}\nState Name:{self.state}\nCountry name: {self.country}")
+class EmployeeDetails(Employee,Address):
+    def __init__(self,name,Id,position,street,state,country):
+        super().__init__(name,Id,position)#ipdiyum panalam
+        Address.__init__(self,street,state,country)#or ipdiyum pannalam any one only
+    def displayEmp(self):
+        self.displayEmployeeInfo()
+        self.displayaddressInfo()
+ed=EmployeeDetails("Jiya",100,"Manager","Shenoy Nagar","TamilNadu","India")
+ed.displayEmp()
+        
